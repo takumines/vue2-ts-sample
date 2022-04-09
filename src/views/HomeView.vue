8caa3e9b731f2d8ac2357abd2bde2bfd0c0eb4af
takumi_kurogi <takumines1@gmail.com>
2022-04-09 19:31:33 +0900

    v-for練習

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,25 +10,49 @@
       <ResetButton v-model="greetText"></ResetButton>
     </p>
     <p>
-      <v-btn>テスト</v-btn>
+      <v-btn @click="addTestDataFunc">push</v-btn>
     </p>
+    <l-map
+      ref="map"
+      style="height: 600px"
+      :zoom="map.zoom"
+      :center="map.center"
+    >
+      <l-tile-layer :url="map.url"></l-tile-layer>
+    </l-map>
+    <ul v-for="eachData in testData" :key="eachData.id">
+      <li>{{ eachData.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Watch, Vue } from "vue-property-decorator";
+import { LMap, LTileLayer } from "vue2-leaflet";
+import "leaflet/dist/leaflet.css";
 import MyButton from "@/components/MyButton.vue";
 import ResetButton from "@/components/ResetButton.vue";
+
+type DataText = { [index: string]: string | number };
+type DataList = DataText[] | [];
 
 @Component({
   components: {
     MyButton,
     ResetButton,
+    LMap,
+    LTileLayer,
   },
 })
 export default class HomeView extends Vue {
   private count = 0;
   public greetText = "Hello";
+  public testData: DataList = [];
+  public map = {
+    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    zoom: 8,
+    center: [34, 137],
+  };
 
   public get isRegulars(): boolean {
     return this.count >= 5;
@@ -44,6 +68,24 @@ export default class HomeView extends Vue {
   public onMyButtonClicked(count: number) {
     this.count = count;
     this.greetText = "こんにちは";
+  }
+
+  public addTestDataFunc() {
+    if (this.testData.length === 0) {
+      let dataList = [];
+      let dataText: DataText;
+      for (let n = 0; n < 5; n++) {
+        dataText = {
+          id: n,
+          name: "name" + n,
+        };
+        dataList.push(dataText);
+        dataText = {};
+      }
+      this.testData = dataList;
+    } else {
+      this.testData = [];
+    }
   }
 }
 </script>
