212fc15ab98d358b04fec66fb7d97eb619a45ee6
takumi_kurogi <takumines1@gmail.com>
2022-04-14 00:21:23 +0900

    ts-ignoreは必要だ

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import vuetify from "./plugins/vuetify";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import L from "leaflet";
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
